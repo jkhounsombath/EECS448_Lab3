8fa96936d412b2caf8a29a0f2270f49276f2717f
Justin Khounsombath <jkhounsombath@ku.edu>
2019-09-24 14:39:03 -0500

    got the img working. Need to find a way to check value of src.

---
+++ slideShow/slide.js
@@ -0,0 +1,47 @@
+function prev()
+{
+  if(document.getElementById("img").src==" https://brandmark.io/logo-rank/random/bp.png ")
+  {
+    document.getElementById("img").src="https://brandmark.io/logo-rank/random/twitter.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/twitter.png")
+  {
+    document.getElementById("img").src= "https://brandmark.io/logo-rank/random/snap.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/snap.png")
+  {
+    document.getElementById("img").src= "https://brandmark.io/logo-rank/random/pepsi.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/pepsi.png")
+  {
+    document.getElementById("img").src="https://brandmark.io/logo-rank/random/yc.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/yc.png")
+  {
+    document.getElementById("img").src="https://brandmark.io/logo-rank/random/bp.png"
+  }
+}
+
+function nex()
+{
+  if(document.getElementById("img").src==" https://brandmark.io/logo-rank/random/bp.png ")
+  {
+    document.getElementById("img").src="https://brandmark.io/logo-rank/random/yc.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/yc.png")
+  {
+    document.getElementById("img").src= "https://brandmark.io/logo-rank/random/pepsi.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/pepsi.png")
+  {
+    document.getElementById("img").src= "https://brandmark.io/logo-rank/random/snap.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/snap.png")
+  {
+    document.getElementById("img").src="https://brandmark.io/logo-rank/random/twitter.png"
+  }
+  else if(document.getElementById("img").src == "https://brandmark.io/logo-rank/random/twitter.png")
+  {
+    document.getElementById("img").src="https://brandmark.io/logo-rank/random/bp.png"
+  }
+}
